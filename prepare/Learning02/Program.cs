e18a5d7c38723c9795f7863d3dbf22dbdcf1b21b
devemmanuel1 <emmanuelezechukwu0@gmail.com>
2024-01-23 08:13:49 +0100

    w2 Assignments

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Project Manager";
+        job2._company = "Canonical";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._personName = "Ezechukwu Emmanuel C.";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
